b120724ac97447bfc2e174aa6dfde12e57d9ea6f
mkrzyzyn <maciej.krzyzynski@gmail.com>
2021-10-06 14:08:06 +0200

    Some http and server code.

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -3,7 +3,7 @@ package advisor;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         new Menu().Actions();
     }
 }
