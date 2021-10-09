38969612800ab6d240ad9dc8088148f3f591ee22
Maciej Krzyżyński <maciej.krzyzynski@gmail.com>
2021-10-09 12:39:21 +0200

    Update Credentials.java

--- Music Advisor/task/src/advisor/Credentials.java
+++ Music Advisor/task/src/advisor/Credentials.java
@@ -3,8 +3,8 @@ package advisor;
 import java.net.URI;
 
 public class Credentials {
-    public static String clientId = "f8a586cd8fa2403fb773606be4f609ab";
-    public static String clientSecret = "96fd4bf58c1e412ebe7ff83f26c37308";
+    public static String clientId = "";
+    public static String clientSecret = "";
     public static String redirectURI = "http://127.0.0.1:8091";
     public static String grantType = "authorization_code";
     public static String code = "";
