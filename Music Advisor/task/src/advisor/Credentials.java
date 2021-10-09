76fae9dacd752f9e3baf0ef059be799fe6aea449
Maciej Krzyżyński <maciej.krzyzynski@gmail.com>
2021-10-09 12:16:48 +0200

    Update Credentials.java

--- Music Advisor/task/src/advisor/Credentials.java
+++ Music Advisor/task/src/advisor/Credentials.java
@@ -3,13 +3,17 @@ package advisor;
 import java.net.URI;
 
 public class Credentials {
+    
     public static String clientId = "";
     public static String clientSecret = "";
-    public static String redirectURI = "http://127.0.0.1:8090";
+    public static String redirectURI = "http://127.0.0.1:8091";
     public static String grantType = "authorization_code";
     public static String code = "";
+    public static String token = "";
+    public static String apiAuthAdressDef = "https://accounts.spotify.com";
+    public static String apiResAdressDef = "https://api.spotify.com";
 
-    public static URI spotifyAuthorizationPage = URI.create("https://accounts.spotify.com/authorize?client_id="
+    public static URI spotifyAuthorizationPage = URI.create(apiAuthAdressDef + "/authorize?client_id="
             + clientId + "&redirect_uri="
             + redirectURI + "&response_type=code");
 
